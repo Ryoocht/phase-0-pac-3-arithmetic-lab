650a69d3422014314a0c55f20f7b832d00a08df2
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-08 19:21:35 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function increment(n){
+    return ++n;
+}
+
+function decrement(n){
+    return --n;
+}
+
+function makeInt(n){
+    let int = parseInt(n, 10);
+    return int;
+}
+
+function preserveDecimal(n){
+    let int = parseFloat(n);
+    return int;
+}
